283576516adaef8c963414a27e9cf8bb13efe64d
Jason <jasonc10kh@gmail.com>
2023-07-30 07:54:22 +0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+//外部套件
+import axios from "axios";
+
+import logo from "./assets/logo.svg";
+import "./assets/App.css";
+import Input from "./components/Input";
+import "./assets/all.scss";
 
 function App() {
+  const [text, setText] = useState("initial");
+
+  const onChangeHandler = (e) => {
+    setText(e.target.value);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const path = process.env.REACT_APP_PATH;
+      const result = await axios.get(path);
+      console.log(result);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          repellat cum dignissimos dolores distinctio voluptatum facilis
+          reiciendis aspernatur in voluptatem quam, corporis doloribus nobis
+          natus placeat, iure, quibusdam dolorum dolorem?
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +41,17 @@ function App() {
         >
           Learn React
         </a>
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+
+        {text}
+        <Input
+          id="sampleText"
+          text="這是一個input"
+          value={text}
+          onChangeHandler={onChangeHandler}
+        />
       </header>
     </div>
   );
